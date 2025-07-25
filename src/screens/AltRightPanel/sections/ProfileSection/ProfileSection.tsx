d7273da6972d9de1327ee3f5e43b80715fa48067
jeremy-eca <jeremy.snyman@eca-international.com>
2025-07-25 14:05:25 +0100

    Fix add address button and Google API lookup

--- src/screens/AltRightPanel/sections/ProfileSection/ProfileSection.tsx
+++ src/screens/AltRightPanel/sections/ProfileSection/ProfileSection.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button } from "../../../../components/ui/button";
+import { MapPinIcon, HomeIcon, Edit2Icon, SaveIcon, XIcon, PlusIcon, SearchIcon } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "../../../../components/ui/card";
 import { Input } from "../../../../components/ui/input";
 import { Textarea } from "../../../../components/ui/textarea";
