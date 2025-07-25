85b1fb862d1204d03d34c1fea7af210ace2190dd
jeremy-eca <jeremy.snyman@eca-international.com>
2025-07-25 16:46:45 +0100

    Updated ProfileSection.tsx

--- src/screens/AltRightPanel/sections/ProfileSection/ProfileSection.tsx
+++ src/screens/AltRightPanel/sections/ProfileSection/ProfileSection.tsx
@@ -4,7 +4,8 @@ import { Card, CardContent, CardHeader, CardTitle } from "../../../../components
 import { MapPinIcon, PlusIcon, Edit2Icon, SaveIcon, XIcon, CheckIcon } from "lucide-react";
 import { Textarea } from "../../../../components/ui/textarea";
 import { Badge } from "../../../../components/ui/badge";
-import { Edit2Icon, SaveIcon, XIcon, CameraIcon, UserIcon, SearchIcon, MapPinIcon, PlusIcon, Trash2Icon } from "lucide-react";
+import { Edit2Icon, SaveIcon, XIcon, CameraIcon, UserIcon, SearchIcon, MapPinIcon, PlusIcon } from "lucide-react";
+import { ProfileSectionType } from "../../AltRightPanel";
 import { PersonProfile } from "../../../../types/frontend.types";
 
 interface ProfileSectionProps {
