c4a7494bd7f1edd2d125825b67b49a09c43a47a4
jeremy-eca <jeremy.snyman@eca-international.com>
2025-07-26 09:26:43 +0100

    Add missing Button component import

--- src/screens/AltRightPanel/sections/ProfileSection/ProfileSection.tsx
+++ src/screens/AltRightPanel/sections/ProfileSection/ProfileSection.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "../../../../components/ui/card";
 import { Input } from "../../../../components/ui/input";
+import { Button } from "../../../../components/ui/button";
 import { Textarea } from "../../../../components/ui/textarea";
 import { Badge } from "../../../../components/ui/badge";
 import { LanguageEditor } from "../../../../components/ui/language-editor";
